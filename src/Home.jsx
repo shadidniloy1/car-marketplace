9f0b848d92ecef38073e4fce90bf6dd6cc78e9a1
Shadid Hasan <shadidhasan1@gmail.com>
2024-10-05 23:02:53 +0600

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/Header'
+
+const Home = () => {
+  return (
+    <div>
+        {/* header  */}
+        <Header/>
+        {/* hero  */}
+    </div>
+  )
+}
+
+export default Home
